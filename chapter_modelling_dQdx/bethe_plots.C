fcb77d49ba6f83761cca9e532d512364a7abb5b4
Jack Anthony <jackanth3@gmail.com>
2018-12-05 19:47:02 +0000

    Added density correction error plots.

--- chapter_modelling_dQdx/bethe_plots.C
+++ chapter_modelling_dQdx/bethe_plots.C
@@ -17,6 +17,10 @@ TCanvas * PlotEnergyMean(const bf::Propagator &propagator, const std::shared_ptr
 TCanvas * PlotEnergies(const bf::Propagator &propagator, const bf::Propagator::PROPAGATION_MODE mode);
 TCanvas * PlotdEdxVersusX(const bf::Propagator &propagator, const bf::Propagator::PROPAGATION_MODE mode);
 TCanvas * PlotdEdxVersusT(const bf::Propagator &propagator, const bf::Propagator::PROPAGATION_MODE mode);
+TGraph GetNoDensityEffectErrorGraph(const bf::Detector &detector, const bf::Propagator &propagator, const std::shared_ptr<bf::Particle> &spParticle);
+TCanvas * PlotNoDensityEffectError(const bf::Detector &detector, const bf::Propagator &propagator);
+TGraph GetFermiPlateauErrorGraph(const bf::Detector &detector, const bf::Propagator &propagator, const std::shared_ptr<bf::Particle> &spParticle);
+TCanvas * PlotFermiPlateauError(const bf::Detector &detector, const bf::Propagator &propagator);
 TCanvas * PlotOverlayGraph(const bf::Propagator &propagator, const std::shared_ptr<bf::Particle> &spParticle, const unsigned int colour, const std::string &darkColour, const std::string &label);
 TCanvas * GetNewCanvas(const std::size_t width = 800UL, const std::size_t height = 600UL);
 
@@ -43,6 +47,11 @@ int bethe_plots()
     PlotdEdxVersusX(propagator, bf::Propagator::PROPAGATION_MODE::MODAL)->SaveAs("modal_dEdxVersusX_mode.eps");
     PlotdEdxVersusT(propagator, bf::Propagator::PROPAGATION_MODE::MODAL)->SaveAs("modal_dEdxVersusT_mode.eps");
 
+    // // Density effect discussion
+    PlotNoDensityEffectError(detector, propagator)->SaveAs("densityeffect_error_nodensityeffect.eps");
+    PlotFermiPlateauError(detector, propagator)->SaveAs("densityeffect_error_fermiplateau.eps");
+
+    // Change formatting for the overlay graphs
     TStyle *pStyle = gROOT->GetStyle("BetheFasterStyle");
 
     pStyle->SetLabelSize(0.065f, "xyz");
@@ -52,6 +61,7 @@ int bethe_plots()
     pStyle->SetPadTopMargin(0.1f);
     pStyle->SetPadRightMargin(0.08f);
 
+    // Overlay graphs for modal energy loss discussion
     PlotOverlayGraph(propagator, bf::ParticleHelper::GetMuon(), 0UL, "#178262", "\\mu")->SaveAs("modal_overlay_muon.eps");
     PlotOverlayGraph(propagator, bf::ParticleHelper::GetChargedPion(), 1UL, "#B24E02", "\\pi^\\pm")->SaveAs("modal_overlay_charged_pion.eps");
     PlotOverlayGraph(propagator, bf::ParticleHelper::GetChargedKaon(), 2UL, "#605C93", "K^\\pm")->SaveAs("modal_overlay_charged_kaon.eps");
@@ -209,6 +219,164 @@ TCanvas * PlotdEdxVersusT(const bf::Propagator &propagator, const bf::Propagator
     pMultiGraph->GetXaxis()->SetLimits(0., 2000.);
     pMultiGraph->SetMinimum(1.);
     pMultiGraph->SetMaximum(5.);
+
+    pMultiGraph->DrawClone("A");
+    pLegend->DrawClone();
+
+    return pCanvas;
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+TGraph GetNoDensityEffectErrorGraph(const bf::Detector &detector, const bf::Propagator &propagator, const std::shared_ptr<bf::Particle> &spParticle)
+{
+    std::vector<double> energies, noDensityEffectErrors;
+    const auto &        history = spParticle->GetHistory();
+
+    if (history.empty())
+        throw std::runtime_error{"Cannot plot empty particle history"};
+
+    for (const auto &spState : history)
+    {
+        const double densityCorrection = propagator.DensityCorrection(spParticle->Mass(), spState->GetKineticEnergy());
+        const double beta = bf::ParticleHelper::GetParticleBeta(spParticle->Mass(), spState->GetKineticEnergy());
+        const double gamma = bf::ParticleHelper::GetParticleGamma(spParticle->Mass(), spState->GetKineticEnergy());
+        const double xi = propagator.Xi(spParticle->Mass(), spState->GetKineticEnergy(), spState->Getdx());
+        const double denominator = std::log(2. * bf::PhysicalConstants::m_electronMass * beta * beta * gamma * gamma * xi / (detector.m_avgIonizationEnergy * detector.m_avgIonizationEnergy * 1.e-12)) + 0.2 - beta * beta - densityCorrection;
+
+        const double noDensityEffectError = std::abs(densityCorrection / denominator);
+
+        energies.push_back(spState->GetKineticEnergy());
+        noDensityEffectErrors.push_back(noDensityEffectError);
+    }
+
+    return TGraph{static_cast<Int_t>(history.size()), energies.data(), noDensityEffectErrors.data()};
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+TCanvas * PlotNoDensityEffectError(const bf::Detector &detector, const bf::Propagator &propagator)
+{
+    // Get the particles
+    const auto &spMuon = bf::ParticleHelper::GetMuon();
+    const auto &spChargedPion = bf::ParticleHelper::GetChargedPion();
+    const auto &spChargedKaon = bf::ParticleHelper::GetChargedKaon();
+    const auto &spProton = bf::ParticleHelper::GetProton();
+
+    // Propagate the particles
+    while ((spMuon->KineticEnergy() < 10000.) && !spMuon->HasFailed())
+        propagator.PropagateBackwards(spMuon, 0.03, bf::Propagator::PROPAGATION_MODE::MODAL);
+
+    while ((spChargedPion->KineticEnergy() < 10000.) && !spChargedPion->HasFailed())
+        propagator.PropagateBackwards(spChargedPion, 0.03, bf::Propagator::PROPAGATION_MODE::MODAL);
+
+    while ((spChargedKaon->KineticEnergy() < 10000.) && !spChargedKaon->HasFailed())
+        propagator.PropagateBackwards(spChargedKaon, 0.03, bf::Propagator::PROPAGATION_MODE::MODAL);
+
+    while ((spProton->KineticEnergy() < 10000.) && !spProton->HasFailed())
+        propagator.PropagateBackwards(spProton, 0.03, bf::Propagator::PROPAGATION_MODE::MODAL);
+
+    // Get the graphs
+    auto muonGraph = bf::MultiGraphEntry{GetNoDensityEffectErrorGraph(detector, propagator, spMuon), "\\mu", 0UL, true, 2};
+    auto chargedPionGraph = bf::MultiGraphEntry{GetNoDensityEffectErrorGraph(detector, propagator, spChargedPion), "\\pi^\\pm", 1UL, true, 2};
+    auto chargedKaonGraph = bf::MultiGraphEntry{GetNoDensityEffectErrorGraph(detector, propagator, spChargedKaon), "K^\\pm", 2UL, true, 2};
+    auto protonGraph = bf::MultiGraphEntry{GetNoDensityEffectErrorGraph(detector, propagator, spProton), "p\\", 3UL, true, 2};
+
+    // Create the graphs
+    bf::PlotOptions options;
+    options.m_xAxisTitle = "T \\text{ (MeV)}";
+    options.m_yAxisTitle = "R_0(T)\\";
+    options.m_legendX1 = 0.14;
+    options.m_legendX2 = 0.22;
+
+    TCanvas *pCanvas = GetNewCanvas();
+
+    TMultiGraph *pMultiGraph = nullptr;
+    TLegend *pLegend = nullptr;
+
+    auto graphs = std::vector<std::reference_wrapper<bf::MultiGraphEntry>>{muonGraph, chargedPionGraph, chargedKaonGraph, protonGraph};
+    bf::PlotHelper::GetMultiGraph(graphs, options, pMultiGraph, pLegend);
+
+    pMultiGraph->GetXaxis()->SetLimits(0., 10000.);
+
+    pMultiGraph->DrawClone("A");
+    pLegend->DrawClone();
+
+    return pCanvas;
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+TGraph GetFermiPlateauErrorGraph(const bf::Detector &detector, const bf::Propagator &propagator, const std::shared_ptr<bf::Particle> &spParticle)
+{
+    std::vector<double> energies, fermiPlateauErrors;
+    const auto &        history = spParticle->GetHistory();
+
+    if (history.empty())
+        throw std::runtime_error{"Cannot plot empty particle history"};
+
+    for (const auto &spState : history)
+    {
+        const double densityCorrection = propagator.DensityCorrection(spParticle->Mass(), spState->GetKineticEnergy());
+        const double beta = bf::ParticleHelper::GetParticleBeta(spParticle->Mass(), spState->GetKineticEnergy());
+        const double gamma = bf::ParticleHelper::GetParticleGamma(spParticle->Mass(), spState->GetKineticEnergy());
+        const double xi = propagator.Xi(spParticle->Mass(), spState->GetKineticEnergy(), spState->Getdx());
+
+        const double numerator = densityCorrection - 2. * std::log(beta * gamma * detector.m_plasmaEnergy / detector.m_avgIonizationEnergy) + beta * beta;
+        const double denominator = std::log(2. * bf::PhysicalConstants::m_electronMass * beta * beta * gamma * gamma * xi / (detector.m_avgIonizationEnergy * detector.m_avgIonizationEnergy * 1.e-12)) + 0.2 - beta * beta - densityCorrection;
+
+        const double fermiPlateauError = std::abs(numerator / denominator);
+
+        energies.push_back(spState->GetKineticEnergy());
+        fermiPlateauErrors.push_back(fermiPlateauError);
+    }
+
+    return TGraph{static_cast<Int_t>(history.size()), energies.data(), fermiPlateauErrors.data()};
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+TCanvas * PlotFermiPlateauError(const bf::Detector &detector, const bf::Propagator &propagator)
+{
+    // Get the particles
+    const auto &spMuon = bf::ParticleHelper::GetMuon();
+    const auto &spChargedPion = bf::ParticleHelper::GetChargedPion();
+    const auto &spChargedKaon = bf::ParticleHelper::GetChargedKaon();
+    const auto &spProton = bf::ParticleHelper::GetProton();
+
+    // Propagate the particles
+    while ((spMuon->KineticEnergy() < 10000.) && !spMuon->HasFailed())
+        propagator.PropagateBackwards(spMuon, 0.03, bf::Propagator::PROPAGATION_MODE::MODAL);
+
+    while ((spChargedPion->KineticEnergy() < 10000.) && !spChargedPion->HasFailed())
+        propagator.PropagateBackwards(spChargedPion, 0.03, bf::Propagator::PROPAGATION_MODE::MODAL);
+
+    while ((spChargedKaon->KineticEnergy() < 10000.) && !spChargedKaon->HasFailed())
+        propagator.PropagateBackwards(spChargedKaon, 0.03, bf::Propagator::PROPAGATION_MODE::MODAL);
+
+    while ((spProton->KineticEnergy() < 10000.) && !spProton->HasFailed())
+        propagator.PropagateBackwards(spProton, 0.03, bf::Propagator::PROPAGATION_MODE::MODAL);
+
+    // Get the graphs
+    auto muonGraph = bf::MultiGraphEntry{GetFermiPlateauErrorGraph(detector, propagator, spMuon), "\\mu", 0UL, true, 2};
+    auto chargedPionGraph = bf::MultiGraphEntry{GetFermiPlateauErrorGraph(detector, propagator, spChargedPion), "\\pi^\\pm", 1UL, true, 2};
+    auto chargedKaonGraph = bf::MultiGraphEntry{GetFermiPlateauErrorGraph(detector, propagator, spChargedKaon), "K^\\pm", 2UL, true, 2};
+    auto protonGraph = bf::MultiGraphEntry{GetFermiPlateauErrorGraph(detector, propagator, spProton), "p\\", 3UL, true, 2};
+
+    // Create the graphs
+    bf::PlotOptions options;
+    options.m_xAxisTitle = "T \\text{ (MeV)}";
+    options.m_yAxisTitle = "R_F(T)\\";
+
+    TCanvas *pCanvas = GetNewCanvas();
+
+    TMultiGraph *pMultiGraph = nullptr;
+    TLegend *pLegend = nullptr;
+
+    auto graphs = std::vector<std::reference_wrapper<bf::MultiGraphEntry>>{muonGraph, chargedPionGraph, chargedKaonGraph, protonGraph};
+    bf::PlotHelper::GetMultiGraph(graphs, options, pMultiGraph, pLegend);
+
+    pMultiGraph->GetXaxis()->SetLimits(0., 10000.);
 
     pMultiGraph->DrawClone("A");
     pLegend->DrawClone();
