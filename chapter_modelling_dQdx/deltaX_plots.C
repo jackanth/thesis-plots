9caab53af8a67c65809e0ebf8fea69bc0d68968d
Jack Anthony <jackanth3@gmail.com>
2018-12-15 12:29:49 +0000

    Added recombination plots.

--- chapter_modelling_dQdx/deltaX_plots.C
+++ chapter_modelling_dQdx/deltaX_plots.C
@@ -15,6 +15,8 @@ R__LOAD_LIBRARY(libbethe-faster.so)
 
 TCanvas * PlotThreeDDeltaX();
 TCanvas * PlotTwoDDeltaX();
+TCanvas * PlotRecombinationR();
+TCanvas * PlotRecombinationInvR(const double lowX, const double highX, const double lowY, const double highY);
 TCanvas * GetNewCanvas(const std::size_t width = 800UL, const std::size_t height = 600UL);
 
 std::size_t g_canvasCounter = 0UL;
@@ -30,6 +32,9 @@ int deltaX_plots()
     const auto propagator = bf::Propagator{detector};
 
     PlotTwoDDeltaX()->SaveAs("deltaX_twoDdeltaX.eps");
+
+    PlotRecombinationR()->SaveAs("recomb_R.eps");
+    PlotRecombinationInvR(0., 14., -50., 200.)->SaveAs("recomb_invR.eps");
 
     gStyle->SetPalette(kBrownCyan);
     PlotThreeDDeltaX()->SaveAs("deltaX_threeDdeltaX.eps");
@@ -105,6 +110,97 @@ TCanvas * PlotTwoDDeltaX()
     legend.AddEntry(&sinTermFunction, "\\frac{w_i}{|\\sin \\phi_i|}", "l");
     legend.DrawClone("same");
 
+    return pCanvas;
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+TCanvas * PlotRecombinationR()
+{
+    TCanvas *pCanvas = GetNewCanvas();
+
+    const double birksA = 0.8;
+    const double recombK = 0.0468;
+    const double recombRho = 1.4;
+    const double recombEpsilon = 0.273;
+
+    TF1 birksFunction{"birksFunction", "[0] / (1. + [1] / ([2] * [3]) * x)", 0., 18.};
+    birksFunction.SetParameter(0, birksA);
+    birksFunction.SetParameter(1, recombK);
+    birksFunction.SetParameter(2, recombRho);
+    birksFunction.SetParameter(3, recombEpsilon);
+    birksFunction.SetLineColor(bf::PlotHelper::GetSchemeColour(0UL));
+    TF1 *pCopyFunc = birksFunction.DrawCopy();
+
+    pCopyFunc->GetYaxis()->SetTitle("R\\");
+    pCopyFunc->GetXaxis()->SetTitle("\\left(-\\mathrm{d}E/\\mathrm{d}x\\right)_\\text{true} \\text{ (MeV/cm)}");
+    pCopyFunc->SetMinimum(0.);
+    pCopyFunc->SetMaximum(1.);
+    pCopyFunc->GetXaxis()->SetRangeUser(0., 18.);
+
+    const double modboxA = 0.93;
+    const double modboxB = 0.212;
+
+    TF1 modBoxFunction{"modBoxFunction", "std::log([0] + [1] / ([2] * [3]) * x) / ([1] / ([2] * [3]) * x)", 0., 100.};
+    modBoxFunction.SetParameter(0, modboxA);
+    modBoxFunction.SetParameter(1, modboxB);
+    modBoxFunction.SetParameter(2, recombRho);
+    modBoxFunction.SetParameter(3, recombEpsilon);
+    modBoxFunction.SetLineColor(bf::PlotHelper::GetSchemeColour(0UL));
+    modBoxFunction.SetLineStyle(2);
+    modBoxFunction.DrawClone("same");
+
+    TLegend legend{0.63, 0.78, 0.88, 0.86};
+    legend.SetBorderSize(1);
+    legend.AddEntry(&birksFunction, "Birks' model", "l");
+    legend.AddEntry(&modBoxFunction, "Modified box model", "l");
+    legend.DrawClone("same");
+
+    return pCanvas;
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+TCanvas * PlotRecombinationInvR(const double lowX, const double highX, const double lowY, const double highY)
+{
+    TCanvas *pCanvas = GetNewCanvas();
+
+    const double birksA = 0.8;
+    const double recombK = 0.0468;
+    const double recombRho = 1.4;
+    const double recombEpsilon = 0.273;
+
+    TF1 birksFunction{"birksFunction", "x / ([0] - [1] / ([2] * [3]) * x)", lowX, highX};
+    birksFunction.SetParameter(0, birksA);
+    birksFunction.SetParameter(1, recombK);
+    birksFunction.SetParameter(2, recombRho);
+    birksFunction.SetParameter(3, recombEpsilon);
+    birksFunction.SetLineColor(bf::PlotHelper::GetSchemeColour(0UL));
+    TF1 *pCopyFunc = birksFunction.DrawCopy();
+
+    pCopyFunc->GetYaxis()->SetTitle("\\left(-\\mathrm{d}E/\\mathrm{d}x\\right)_\\text{true} \\text{ (MeV/cm)}");
+    pCopyFunc->GetXaxis()->SetTitle("\\left(-\\mathrm{d}E/\\mathrm{d}x\\right)_\\text{apparent} \\text{ (MeV/cm)}");
+    pCopyFunc->SetMinimum(lowY);
+    pCopyFunc->SetMaximum(highY);
+
+    const double modboxA = 0.93;
+    const double modboxB = 0.212;
+
+    TF1 modBoxFunction{"modBoxFunction", "(std::exp([1] / ([2] * [3]) * x) - [0])/([1] / ([2] * [3]))", lowX, highX};
+    modBoxFunction.SetParameter(0, modboxA);
+    modBoxFunction.SetParameter(1, modboxB);
+    modBoxFunction.SetParameter(2, recombRho);
+    modBoxFunction.SetParameter(3, recombEpsilon);
+    modBoxFunction.SetLineColor(bf::PlotHelper::GetSchemeColour(0UL));
+    modBoxFunction.SetLineStyle(2);
+    modBoxFunction.DrawClone("same");
+
+    TLegend legend{0.63, 0.78, 0.88, 0.86};
+    legend.SetBorderSize(1);
+    legend.AddEntry(&birksFunction, "Birks' model", "l");
+    legend.AddEntry(&modBoxFunction, "Modified box model", "l");
+    legend.DrawClone("same");
+    
     return pCanvas;
 }
 
